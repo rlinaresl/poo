23dd6ba88eae33222c92b6407265b217dc49410e
JuanMiguel <jmigueldr@gmail.com>
2012-07-26 16:22:23 -0500

    avance compras

---
+++ test/testDominio/CompraTest.java
@@ -0,0 +1,46 @@
+package testDominio;
+
+import static org.junit.Assert.assertEquals;
+import java.util.Date;
+import junit.framework.Assert;
+import org.junit.Test;
+
+import controlador.ClienteControlador;
+import controlador.UsuarioControlador;
+import controlador.CompraControlador;
+import dominio.Cliente;
+import dominio.OperacionDetalle;
+import dominio.Usuario;
+import dominio.OperacionCabecera;
+import dominio.Util;
+
+public class CompraTest {
+	
+	@Test
+    public void crearCompraTest() {        
+        
+        ClienteControlador clienteCo = new ClienteControlador();
+        Cliente cli = clienteCo.BuscarCliente("0610430");
+        
+        UsuarioControlador usuarioCo = new UsuarioControlador();
+        Usuario usu = usuarioCo.BuscarUsuario(1);        
+        
+        OperacionCabecera compraCabe = null;
+        Date fechaVencimiento = Util.getFecha("01/10/2012");
+        Date fechaPago = Util.getFecha("01/10/2012");        
+        Date fechaEmision = Util.getFecha("01/10/2012");               
+                       
+        OperacionDetalle compraDeta = null;
+        
+        compraDeta = new OperacionDetalle("servicio de consultoria", 400, 1, 72, 472);
+        compraCabe = new OperacionCabecera(1, 1, cli, 1, 1, usu, "VE00001", fechaEmision, "", fechaVencimiento, fechaPago, 1, compraDeta);  
+        
+        CompraControlador compraCo = new CompraControlador();
+        int resultado = compraCo.crearCompraCabe(compraCabe);
+        
+        //verifico
+        assertEquals(1, resultado, 0);        
+
+    }
+	
+}
