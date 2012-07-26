8e4e35fad551d808b978a581d1aaf551978022e2
Ronald Quilca Condori <rquilca@comercio.com.pe>
2012-07-26 10:37:21 -0500

    poo

--- test/testDominio/ClienteTest.java
+++ test/testDominio/ClienteTest.java
@@ -1,9 +1,5 @@
 package testDominio;
-
-import static org.junit.Assert.*;
-
 import org.junit.Test;
-
 import dominio.Cliente;
 
 public class ClienteTest {
@@ -22,16 +18,14 @@ public class ClienteTest {
 		String apellidos = "Quilca";
 		int tipoCliente = 3;
 		int codigoTipoDocumento = 3;
-		String numeroDocumento = "";
+		String numeroDocumento = "34234";
 		String telefono="";
 		String correo = "";
 		String direccion = "";
 		String ciudad = "";
 		String estado = "";
-		String pais = "Perú";
-		Cliente c = new Cliente(codigo, nombres, apellidos, tipoCliente, codigoTipoDocumento, numeroDocumento, telefono, correo, direccion, 
-			ciudad, estado, pais);
-		assertEquals("Quilca",c.getApellidos());
+		String pais = "Peru";
+		Cliente c = new Cliente(codigo, nombres, apellidos, tipoCliente, codigoTipoDocumento, numeroDocumento, telefono, correo, direccion, ciudad, estado, pais);
 	}
 	
 	@Test
